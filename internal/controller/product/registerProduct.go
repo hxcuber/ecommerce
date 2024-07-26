6576f0ffe983ff0128348f9b46ecb21ac653637b
Hoang Nguyen <hn622@ic.ac.uk>
2024-07-26 10:59:48 +0700

    feat(product-apis): @hxcuber RegisterUser controller done

--- internal/controller/product/registerProduct.go
+++ internal/controller/product/registerProduct.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"github.com/google/uuid"
 	"github.com/hxcuber/ecommerce/internal/handler/product/request"
+	"github.com/hxcuber/ecommerce/internal/repository"
+	"github.com/hxcuber/ecommerce/internal/repository/orm"
+	"github.com/hxcuber/ecommerce/pkg/logerr"
 	"github.com/hxcuber/ecommerce/pkg/model"
 	"github.com/hxcuber/ecommerce/pkg/util"
+	"log"
 	"time"
 )
 
@@ -19,5 +23,23 @@ func (i impl) RegisterProduct(ctx context.Context, request request.PostProductsR
 		CategoryID:  request.CategoryID,
 		CreatedAt:   time.Time{},
 		UpdatedAt:   time.Time{},
-	},
+	}
+
+	var returnProduct *orm.Product
+	err := i.reg.DoInTx(context.Background(), func(ctx context.Context, registry repository.Registry) error {
+		var err error
+		returnProduct, err = registry.Product().CreateProduct(ctx, product)
+		if err != nil {
+			log.Printf(logerr.LogErrMessage("RegisterProduct", "creating product", err))
+			return err
+		}
+		return nil
+	}, nil)
+
+	if err != nil {
+		log.Printf(logerr.LogErrMessage("RegisterProduct", "doing in transaction", err))
+		return model.Product{}, err
+	}
+
+	return model.FromOrmProduct(returnProduct), nil
 }
